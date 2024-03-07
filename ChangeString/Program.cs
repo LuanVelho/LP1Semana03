7b09ae773eec7614069782b4608af4457addf344
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-03-07 11:37:22 -0500

    Exercício 2 incompleto, foreach não funciona

---
+++ ChangeString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva uma string:");
+            string frase = Console.ReadLine();
+            Console.WriteLine("Escreva uma letra para substituir");
+            string letra = Console.ReadLine();
+            foreach (f in frase)
+                if (f == letra)
+                    Console.Write("X");
+                else (Console.WriteLine(f));
+
+        }
+    }
+}
